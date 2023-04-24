3b04902d61d61c1f6858fd4a5b2944969d3963b1
atad <1585232641@qq.com>
2023-04-24 19:33:52 +0800

    添加Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM golang:1.17 as builder
+
+ENV GO111MODULE=on \
+    GOPROXY=https://goproxy.cn,direct
+
+WORKDIR /app
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -ldflags="-w -s" -o main
+RUN mkdir publish \
+    && cp main publish
+
+FROM busybox:1.28.4
+
+WORKDIR /app
+
+COPY --from=builder /app/publish .
+
+ENV GIN_MODE=release
+
+EXPOSE 8085
+
+ENTRYPOINT ["./main"]
---
+++ setdocker.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+date
+echo "If update StuManager?"
+read x1
+if [ $x1 = 'Y']
+then
+cd go
+rm -rf new
+mkdir new && cd new
+
+git clone https://ghproxy.com/https://github.com/littlepi-bit/FinalDesign.git
+
+cd StuManager
+docker ps -a
+echo "Please input docker container ID: "
+read x
+docker stop $x
+docker rm $x
+docker images
+echo "Please input docker images ID: "
+read y
+docker rmi $y
+echo "Please input image tag: "
+read z
+docker build -t stumanager:$z .
+docker run --name stumanager -p 8085:8085 -d stumanager:$z
+cd ..
+cd ..
+cd ..
+fi
+echo "if update StuFront?"
+read x2
+if [ $x2 = 'Y']
+then
+cd react
+rm -rf new
+mkdir new && cd new
+
+git clone https://ghproxy.com/https://github.com/littlepi-bit/filemanagement.git
+cd StuManagerFront
+yarn
+yarn build
+echo "build again?"
+read b
+if [$b='Y']
+then
+yarn build
+fi
+cd build
+vim index.html
+docker ps -a
+echo "Please input docker container ID: "
+read x
+docker stop $x
+docker rm $x
+docker images
+echo "Please input docker images ID: "
+read y
+docker rmi $y
+echo "Please input image tag: "
+read z
+docker build -t stufront:$z .
+docker run --name stufront -p 8081:8081 -d stufront:$z
+docker ps -a
+
+docker images
+echo "Please input docker image ID"
+fi
